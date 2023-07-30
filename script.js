d933b9aa7fa86952244758e51ac3f7db1a384ec6
Placide11 <placideshema11@gmail.com>
2023-07-30 20:49:48 +0200

    fixed

--- script.js
+++ script.js
@@ -24,41 +24,41 @@ function appendDecimal() {
 }
 
 function appendOperator(selectedOperator) {
-  if (firstNumber === null) {
-    firstNumber = parseFloat(displayValue);
-    operator = selectedOperator;
-    displayValue = '0';
-  } else {
+  if (operator !== null) {
     calculate();
-    operator = selectedOperator;
   }
+  firstNumber = parseFloat(displayValue);
+  operator = selectedOperator;
+  displayValue = '0';
 }
 
 function calculate() {
   const secondNumber = parseFloat(displayValue);
-  switch (operator) {
-    case '+':
-      displayValue = (firstNumber + secondNumber).toString();
-      break;
-    case '-':
-      displayValue = (firstNumber - secondNumber).toString();
-      break;
-    case '*':
-      displayValue = (firstNumber * secondNumber).toString();
-      break;
-    case '/':
-      if (secondNumber === 0) {
-        displayValue = "Error: Cannot divide by zero";
-      } else {
-        displayValue = (firstNumber / secondNumber).toString();
-      }
-      break;
-    default:
-      break;
+  if (operator !== null) {
+    switch (operator) {
+      case '+':
+        displayValue = (firstNumber + secondNumber).toString();
+        break;
+      case '-':
+        displayValue = (firstNumber - secondNumber).toString();
+        break;
+      case '*':
+        displayValue = (firstNumber * secondNumber).toString();
+        break;
+      case '/':
+        if (secondNumber === 0) {
+          displayValue = "Error: Cannot divide by zero";
+        } else {
+          displayValue = (firstNumber / secondNumber).toString();
+        }
+        break;
+      default:
+        break;
+    }
+    firstNumber = parseFloat(displayValue);
+    operator = null;
+    updateDisplay();
   }
-  firstNumber = parseFloat(displayValue);
-  operator = null;
-  updateDisplay();
 }
 
 function clearDisplay() {
